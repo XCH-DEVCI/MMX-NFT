fe73d5ee448c0b16e2e719c1180e667e10d61e62
XCH-DEVCI <sbcservice22217102@outlook.com>
2025-04-15 08:32:04 +0700

    Update nft.js

--- nft.js
+++ nft.js
@@ -1,15 +1,18 @@
 var creator;
 var token_id = 0;
-var tokens = {};  // token_id -> {owner, metadata, royalty_address, royalty_rate}
 
-// Contract constructor
+// token_id => { owner, metadata_url, image_url, royalty_address, royalty_rate }
+var tokens = {};
+
+// Contract constructor: sets the creator address
 function init(creator_str) {
   creator = bech32(creator_str);
 }
 
-// Mint NFT with metadata URI, royalty address, and royalty rate
-function mint(to_str, metadata_uri, royalty_str, rate) public {
+// Mint a new NFT with full metadata, image, and royalty settings
+function mint(to_str, metadata_url, image_url, royalty_str, rate) public {
   assert(this.user == creator, "not authorized");
+
   var to = bech32(to_str);
   var royalty_addr = bech32(royalty_str);
   var royalty_rate = uint(rate);
@@ -19,7 +22,8 @@ function mint(to_str, metadata_uri, royalty_str, rate) public {
 
   set(tokens, id, {
     owner: to,
-    metadata: metadata_uri,
+    metadata_url: metadata_url,
+    image_url: image_url,
     royalty_address: royalty_addr,
     royalty_rate: royalty_rate
   });
@@ -27,7 +31,8 @@ function mint(to_str, metadata_uri, royalty_str, rate) public {
   event("mint", {
     id: id,
     to: string_bech32(to),
-    metadata: metadata_uri,
+    metadata_url: metadata_url,
+    image_url: image_url,
     royalty: {
       to: string_bech32(royalty_addr),
       rate: royalty_rate
@@ -35,40 +40,39 @@ function mint(to_str, metadata_uri, royalty_str, rate) public {
   });
 }
 
-// View token details
+// Get token details (including image and metadata URLs)
 function get_token(id) const public {
-  var t = tokens[id];
-  assert(t != null, "not found");
-  return t;
+  var token = tokens[id];
+  assert(token != null, "token not found");
+  return token;
 }
 
-// Destroy an owned token
+// Burn an NFT (only allowed by owner)
 function burn(id) public {
-  var t = tokens[id];
-  assert(t != null, "not found");
-  assert(t.owner == this.user, "not owner");
+  var token = tokens[id];
+  assert(token != null, "token not found");
+  assert(token.owner == this.user, "not owner");
   erase(tokens, id);
   event("burn", {id: id});
 }
 
-// Buy token: auto pays royalty + seller, transfers NFT
+// Buy an NFT: pays seller and royalty address automatically
 function buy(id) public payable {
-  var t = tokens[id];
-  assert(t != null, "not found");
-  assert(t.owner != this.user, "cannot buy your own");
+  var token = tokens[id];
+  assert(token != null, "token not found");
+  assert(token.owner != this.user, "cannot buy your own token");
 
-  var currency = this.deposit.currency;
-  assert(currency == bech32(), "only MMX accepted");
-
+  assert(this.deposit.currency == bech32(), "only MMX accepted");
   var total = this.deposit.amount;
-  var royalty_amount = (total * t.royalty_rate) / 100;
+
+  var royalty_amount = (total * token.royalty_rate) / 100;
   var seller_amount = total - royalty_amount;
 
-  send(t.owner, seller_amount);
-  send(t.royalty_address, royalty_amount);
+  send(token.owner, seller_amount);
+  send(token.royalty_address, royalty_amount);
 
-  var prev_owner = t.owner;
-  t.owner = this.user;
+  var prev_owner = token.owner;
+  token.owner = this.user;
 
   event("buy", {
     id: id,
